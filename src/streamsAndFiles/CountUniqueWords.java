b0865deb1fa1c0ed4ce19ad5544b39b335df3487
Georgi <hristovgeorgivalentinov@gmail.com>
2024-06-17 15:16:01 +0300

    count unique words from file

--- src/streamsAndFiles/CountUniqueWords.java
+++ src/streamsAndFiles/CountUniqueWords.java
@@ -1,2 +1,37 @@
-package streamsAndFiles;public class CountUniqueWords {
+package streamsAndFiles;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class CountUniqueWords {
+    public static void main(String[] args) {
+
+        File file = new File("src/resources/input.txt");
+        Set<String> uniqueWordsSet = new HashSet<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+
+            String line = reader.readLine();
+
+            while (line != null) {
+
+                uniqueWordsSet.addAll(Arrays.asList(line.split("[.*\\s+]+")));
+
+                line = reader.readLine();
+            }
+
+
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
+        }
+
+        System.out.println("Unique words: " + uniqueWordsSet.size());
+
+    }
+
 }
