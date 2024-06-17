4619dee8a02158dfd75c298f77b6f64b4b55c93b
Georgi <hristovgeorgivalentinov@gmail.com>
2024-06-17 10:30:19 +0300

    character frequency exercise

---
+++ src/streamsAndFiles/CharacterFrequency.java
@@ -0,0 +1,45 @@
+package streamsAndFiles;
+
+import java.io.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class CharacterFrequency {
+    public static void main(String[] args){
+
+        File file = new File("src/resources/input.txt");
+
+        Map<Character, Integer> charMap = new LinkedHashMap<>();
+
+        try (BufferedReader bf = new BufferedReader(new FileReader(file))) {
+
+            int currentChar = bf.read();
+
+            while (currentChar != -1) {
+
+                char symbol = (char) currentChar;
+
+                if (charMap.containsKey(symbol)) {
+                    charMap.put(symbol, charMap.get(symbol) + 1);
+                } else {
+                    charMap.put(symbol, 1);
+                }
+
+                currentChar = bf.read();
+
+            }
+
+        } catch (IOException exception) {
+
+            System.out.println(exception.getMessage());
+
+        }
+
+        for (Map.Entry<Character, Integer> m : charMap.entrySet()) {
+
+            System.out.printf("%c: %d%n", m.getKey(), m.getValue());
+        }
+
+    }
+
+}
