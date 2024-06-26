387005c7dd6b8f79d99c7ba3e193c092acf8a8ef
Georgi <hristovgeorgivalentinov@gmail.com>
2024-06-26 18:56:48 +0300

    implement methods to customize decimal symbols

--- src/inheritanceAndInterfaces/inventoryManagementSystem/dataManipulation/FormatSeparators.java
+++ src/inheritanceAndInterfaces/inventoryManagementSystem/dataManipulation/FormatSeparators.java
@@ -1,2 +1,23 @@
-package inheritanceAndInterfaces.inventoryManagementSystem.dataManipulation;public class FormatSeparators {
+package inheritanceAndInterfaces.inventoryManagementSystem.dataManipulation;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+
+/**
+ * Using this class to set custom decimal format symbols according customer requirements for data format.
+ */
+
+public class FormatSeparators {
+
+    /**
+     * Method set decimal format symbol to '.' (dot symbol).
+     * Return decimal format with dot separator to two decimal signs after decimal point.
+     */
+
+    public static DecimalFormat dotSeparatorTwoSignAfter() {
+
+        DecimalFormatSymbols dfs = new DecimalFormatSymbols();
+        dfs.setDecimalSeparator('.');
+        return new DecimalFormat("0.##", dfs);
+    }
 }
