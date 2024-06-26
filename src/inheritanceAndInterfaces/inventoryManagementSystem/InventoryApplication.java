08f6f96b0d29f790893ba95c9170a451effc0252
Georgi <hristovgeorgivalentinov@gmail.com>
2024-06-26 09:56:19 +0300

    add comments on  data stuctures in InventoryApplication class

--- src/inheritanceAndInterfaces/inventoryManagementSystem/InventoryApplication.java
+++ src/inheritanceAndInterfaces/inventoryManagementSystem/InventoryApplication.java
@@ -1,6 +1,8 @@
 package inheritanceAndInterfaces.inventoryManagementSystem;
 
 import inheritanceAndInterfaces.inventoryManagementSystem.dataManipulation.Data;
+import inheritanceAndInterfaces.inventoryManagementSystem.interfaces.classesImpl.ElectronicItem;
+import inheritanceAndInterfaces.inventoryManagementSystem.interfaces.classesImpl.GroceryItem;
 import inheritanceAndInterfaces.inventoryManagementSystem.interfaces.classesImpl.InventoryItem;
 
 import java.util.ArrayList;
@@ -11,16 +13,30 @@ public class InventoryApplication {
 
     public static void main(String[] args) {
 
+        /*
+         * This list stores inventory items that have incorrect data or are entered in different format.
+         * The information may be used to revise items data in storage file if needed.
+         */
         List<String> wrongDataFormat = new ArrayList<>();
+
+        /*
+        * The list stores expired products and should be used for easier product handling if needed.
+         */
         List<InventoryItem> expiredProductsList = new ArrayList<>();
+
+        /*
+        * Map stores all correctly entered and non expired products from data file.
+         */
         Map<Integer, List<InventoryItem>> inventoryStorageMap = Data.loadItems(wrongDataFormat, expiredProductsList);
 
-        // write inventoryStorageMap values into the itemsData.txt after finish work.
-        // sout wrongData in console
-        // sout expired products
+
+        InventoryItem item = new GroceryItem("grocery", 10.54, 11113, 100, "23.02.2027");
+        boolean is  = item.isBreakable();
+
+        InventoryItem item1 = new ElectronicItem("electronic", 454.45, 22223, 10, 500);
+        boolean isNot = item1.isBreakable();
 
 
-        System.out.println();
     }
 
 
