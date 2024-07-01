adb6ee65ec46078cf60cbeb7a704e50398abb0e5
Georgi <hristovgeorgivalentinov@gmail.com>
2024-07-01 16:22:03 +0300

    implement create methods of ArrayCreator class

--- src/generics/genericArrayCreator/ArrayCreator.java
+++ src/generics/genericArrayCreator/ArrayCreator.java
@@ -1,2 +1,26 @@
-package generics.genericArrayCreator;public class ArrayCreator {
+package generics.genericArrayCreator;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class ArrayCreator {
+
+    public static <T> T[] create(int length, T item) {
+
+        T[] arr = (T[]) Array.newInstance(item.getClass(), length);
+
+        Arrays.fill(arr, item);
+
+        return arr;
+    }
+
+    public static <T> T[] create(Class<T> clazz, int length, T item) {
+
+        T[] arr = (T[]) Array.newInstance(clazz, length);
+
+        Arrays.fill(arr, item);
+
+        return arr;
+    }
+
 }
---
+++ src/generics/genericArrayCreator/Main.java
@@ -0,0 +1,15 @@
+package generics.genericArrayCreator;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int intItem = 10;
+        int length = 5;
+        String strItem = "word";
+
+        Integer[] intArray = ArrayCreator.create(length, intItem);
+
+        String[] strArray = ArrayCreator.create(String.class, length, strItem);
+
+    }
+}
