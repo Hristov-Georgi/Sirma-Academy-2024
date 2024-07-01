5112a6cf4e92ebc4289a95f61e6aa206f9c337bb
Georgi <hristovgeorgivalentinov@gmail.com>
2024-07-01 17:05:17 +0300

    implemented Scale class and getHeavier method

---
+++ src/generics/genericScale/Main.java
@@ -0,0 +1,16 @@
+package generics.genericScale;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int left = 2;
+        int right = 2;
+
+        Scale<Integer> scale = new Scale<>(left, right);
+
+        Integer heavier = scale.getHeavier();
+
+        System.out.println();
+    }
+
+}
---
+++ src/generics/genericScale/Scale.java
@@ -0,0 +1,26 @@
+package generics.genericScale;
+
+public class Scale<T extends Comparable<T>> {
+
+    private T left;
+    private T right;
+
+    public Scale(T left, T right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public T getHeavier() {
+
+        if (this.left.compareTo(this.right) > 0) {
+            return left;
+        } else if (this.left.compareTo(this.right) < 0) {
+            return right;
+        } else {
+            return null;
+        }
+
+    }
+
+
+}
