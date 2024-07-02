a42047cb1590b96d0a4b4713e604d7263db22747
Georgi <hristovgeorgivalentinov@gmail.com>
2024-07-02 14:22:27 +0300

    added CustomList interface and initialized methods

---
+++ src/generics/customList/CustomList.java
@@ -0,0 +1,18 @@
+package generics.customList;
+
+public interface CustomList<T extends Comparable<T>>{
+
+    void add(T element);
+
+    T remove(int index);
+
+    boolean contains(T element);
+
+    void swap(int firstIndex, int secondIndex);
+
+    int countGreaterThan(T element);
+
+    public T getMax();
+
+    public T getMin();
+}
--- src/generics/customList/MyCustomList.java
+++ src/generics/customList/MyCustomList.java
@@ -1,2 +1,40 @@
-package generics.customList;public class MyCustomList {
+package generics.customList;
+
+public class MyCustomList<T extends Comparable<T>> implements CustomList<T> {
+
+
+    @Override
+    public void add(T element) {
+
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean contains(T element) {
+        return false;
+    }
+
+    @Override
+    public void swap(int firstIndex, int secondIndex) {
+
+    }
+
+    @Override
+    public int countGreaterThan(T element) {
+        return 0;
+    }
+
+    @Override
+    public T getMax() {
+        return null;
+    }
+
+    @Override
+    public T getMin() {
+        return null;
+    }
 }
